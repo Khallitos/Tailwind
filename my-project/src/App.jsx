a60a7740d809245c5fb617a09b153e2a33867e03
khallitos <carlphil9924@gmail.com>
2023-10-03 17:00:35 +0000

    Landing Page

---
+++ my-project/src/App.jsx
@@ -0,0 +1,25 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+    PopularProducts
+    </section>
+
+    <section className="padding">
+    SuperQuality
+    </section>
+
+    <section className="padding">
+    Services
+    </section>
+
+    <section className="padding-x py-10">
+    PopularProducts
+    </section>
+   
+  </main>
+)
+export default App;
